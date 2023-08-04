4c0b5e0cca1133bb1005145e9a7bcc748d39c179
somozadev <somozadev@gmail.com>
2023-08-04 03:28:05 +0200

    Temp car instantiation for level generation

--- Racing tT/Assets/Scripts/LevelGenerator.cs
+++ Racing tT/Assets/Scripts/LevelGenerator.cs
@@ -1,19 +1,17 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    [SerializeField]
-    private Grid grid;
-
-    [SerializeField]
-    public LevelTarckovChain chain;
-
-    private Transform currentModule;
-    [SerializeField]
-    private Transform startModule;
+    [SerializeField] private int modulesAmount = 25;
+        
+    [SerializeField] private GameObject carPrefab;
+    [SerializeField] private Grid grid;
+    [SerializeField] public LevelTarckovChain chain;
+    [SerializeField] private Transform startModule;
     private Transform startingModule;
+    private Transform currentModule;
 
     private void Start()
     {
@@ -21,13 +19,20 @@ public class LevelGenerator : MonoBehaviour
         startingModule.transform.position = Vector3.zero;
         grid.addModule(startingModule.position, startingModule.GetComponent<Module>());
         currentModule = startingModule;
+        InstantiateCarAtOrigin();
         GenerateLevel(chain);
     }
 
+    private void InstantiateCarAtOrigin()
+    {
+        var position = startingModule.position;
+        var initialPos = new Vector3( position.x, 15 + 1.5f, position.z); //30 due the size of the module, maybe better to use a variable here to modify the size
+        Instantiate(carPrefab, initialPos, Quaternion.identity);
+    }
     public void GenerateLevel(LevelTarckovChain levelChain)
     {
         Chain c = levelChain.GetChain();
-        for (int i = 0; i < 25; i++)
+        for (int i = 0; i < modulesAmount; i++)
         {
             Transform t = Instantiate(c.Prefab);
             t.position = currentModule.position + c.GetSpawnPoint(currentModule.position).position;
@@ -35,5 +40,6 @@ public class LevelGenerator : MonoBehaviour
             currentModule = t;
             c = c.GetChild();
         }
+        
     }
 }
