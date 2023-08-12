b1a761278b2079560fc03f9461064e1d10876bf7
somozadev <somozadev@gmail.com>
2023-08-12 03:43:39 +0200

    Added bunch of assets

---
+++ Racing tT/Assets/Scripts/Augments/AugmentsBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using GroundEffects;
+using UnityEngine;
+
+namespace Augments
+{
+    public class AugmentsBehaviour : CarTriggerDetection
+    {
+        private void OnEnable()
+        {
+            TriggerEnterEvent += ApplyAgument;
+        }
+
+        private void OnDisable()
+        {
+            TriggerEnterEvent -= ApplyAgument;
+        }
+
+        private void ApplyAgument(Collider other)
+        {
+            
+        }
+    }
+}
